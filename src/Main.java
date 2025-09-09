df4990e853c5fc80fffdcd8369a2a727e2d6bab1
HunterYarmy <huntery5@nycstudents.net>
2025-09-09 15:27:46 -0400

    Added comments.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException{
         System.out.println("What's your name?");
-        Scanner s = new Scanner(System.in);
-        String name = s.nextLine();
+        Scanner s = new Scanner(System.in); //creating scanner
+        String name = s.nextLine(); //assigning variable with input
         System.out.println("Hello, " + name + "!");
 
         File f = new File("Class.txt");
-        s = new Scanner(f);
-        String course = s.nextLine();
+        s = new Scanner(f); //creating scanner
+        String course = s.nextLine(); //adding content of txt file to variable
         System.out.println("Welcome to " + course + "!");
     }
 }
